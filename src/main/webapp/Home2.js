4f01e89c797aa1618647f5688da7ab0caad00f4f
ht325ver0 <sou10aka6@docomo.ne.jp>
2023-10-07 13:36:45 +0900

    zero

---
+++ src/main/webapp/Home2.js
@@ -0,0 +1,76 @@
+let num0 = [];
+let num1 = [];
+
+
+function addnum0(){
+	num0.push(0);
+	alert(num0);
+};
+
+function addnum1(){
+	num0.push(1);
+	alert(num0);
+};
+
+function addnum2(){
+	num0.push(2);
+	alert(num0);
+};
+
+function addnum3(){
+	num0.push(3);
+	alert(num0);
+};
+
+function addnum4(){
+	num0.push(4);
+	alert(num0);
+};
+
+function addnum5(){
+	num0.push(5);
+	alert(num0);
+};
+
+function addnum6(){
+	num0.push(6);
+	alert(num0);
+};
+
+function addnum7(){
+	num0.push(7);
+	alert(num0);
+};
+
+function addnum8(){
+	num0.push(8);
+	alert(num0);
+};
+
+function addnum9(){
+	num0.push(9);
+	alert(num0);
+};
+
+
+let btn1 = document.getElementById('bt1');
+let btn2 = document.getElementById('bt2');
+let btn3 = document.getElementById('bt3');
+let btn4 = document.getElementById('bt4');
+let btn5 = document.getElementById('bt5');
+let btn6 = document.getElementById('bt6');
+let btn7 = document.getElementById('bt7');
+let btn8 = document.getElementById('bt8');
+let btn9 = document.getElementById('bt9');
+let btn0 = document.getElementById('bt0');
+
+btn0.addEventListener('click',addnum0);
+btn1.addEventListener('click',addnum1);
+btn2.addEventListener('click',addnum2);
+btn3.addEventListener('click',addnum3);
+btn4.addEventListener('click',addnum4);
+btn5.addEventListener('click',addnum5);
+btn6.addEventListener('click',addnum6);
+btn7.addEventListener('click',addnum7);
+btn8.addEventListener('click',addnum8);
+btn9.addEventListener('click',addnum9);
---
+++ src/main/webapp/WEB-INF/Dentaku.js
@@ -0,0 +1,33 @@
+var num0 = [];
+let sumlist = [];
+
+function addNum0(num) {
+    num0.push(num);
+}
+
+function plu(num){
+	var str = num.join('');
+	let ele = Number(str);
+	sumlist.push(ele);
+	alert(`数字は${ele}です。`);
+	num0.splice(0,num0.length);
+}
+
+function sum(lis){
+	let total = lis.reduce((sum,element) => sum + element,0);
+	alert(`「${sumlist}」これを足します。`);
+	alert(`合計は${total}です`);
+
+}
+
+for (let i = 0; i <= 11; i++) {
+    let btn = document.getElementById(`bt${i}`);
+    if (i == 10){
+		btn.addEventListener('click',() => plu(num0));
+	} else if (i ==11){
+		btn.addEventListener('click',() => plu(num0));
+		btn.addEventListener('click',() => sum(sumlist));
+	} else{
+		btn.addEventListener('click',() => addNum0(i));
+	}
+}
